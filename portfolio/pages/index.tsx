617360de0a79f3e28bfc8f6d76a1cf51a3cc26bf
Espenjansen <ejansen1201@gmail.com>
2022-11-25 10:06:33 +0100

    update

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <body className='body bg-zinc-800'>
+      <h1 className='h1 text-3xl font-bold text-center text-white'>Dette er min portfolio</h1>
+      </body>
+    </div>
+  )
+}
+
+export default index
