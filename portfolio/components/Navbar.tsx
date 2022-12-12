cadfa06b9ad2984eb8c44fa8da772a49bbb597aa
Espenjansen <ejansen1201@gmail.com>
2022-12-12 12:22:34 +0100

    update navbar

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -3,12 +3,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-zinc-800'>
-        <ul>
-            <li>
-                <Link className='font-semibold text-2xl text-white' href="/om_meg">om meg</Link>
-            </li>
-            <span className='text-white'> | </span>
+    <div className='bg-zinc-800 flex flex-1 items-center justify-end md:justify-between'>
+        <nav>
+        <ul className='flex items-center gap-6 text-sm'>
             <li>
                 <Link className='font-semibold text-2xl text-white' href="/">Hjem</Link>
             </li>
@@ -16,7 +13,12 @@ const Navbar = () => {
             <li>
                 <Link className='font-semibold text-2xl text-white' href="/prosjekter">prosjekter</Link>
             </li>
+            <span className='text-white'> | </span>
+            <li>
+                <Link className='font-semibold text-2xl text-white' href="/om_meg">om meg</Link>
+            </li>
         </ul>
+        </nav>
     </div>
     )
 }
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -7,10 +7,8 @@ import img from '../public/assets/bg.jpg'
 const index = () => (
   <div className='indexdiv min-h-screen bg-zinc-800'>
   {/* <Image className='profilepic border-slate-800' layout='fill' src={img} alt="bg"/> */}
-    <h1 className='<h1 class="h1 absolute text-center bottom-2/3 text-white  font-bold text-3xl'>{content.no.text[2]}</h1>
-    <Link className='projectlink absolute text-center bottom-1/2 left-1/3 font-semibold text-2xl text-white' href="/prosjekter">mine prosjekter</Link>
-    <Link className='projectlink absolute text-center bottom-1/2 left-2/3 font-semibold text-2xl text-white' href="/om_meg">om meg</Link>
-    <h1 className='h1 absolute bottom-1/3 text-center text-xl font-semibold text-white'>{content.no.text[1]} {content.no.text[0]}</h1>
+    <h1 className='text-center text-white  font-bold text-3xl'>{content.no.text[2]}</h1>
+    <h1 className='text-center text-xl font-semibold text-white'>{content.no.text[1]} {content.no.text[0]}</h1>
   </div>
 )
 
