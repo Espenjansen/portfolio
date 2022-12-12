fb35bf64040c224b0e09191abae2717eadfd1b54
Espenjansen <ejansen1201@gmail.com>
2022-12-12 12:28:26 +0100

    update

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  return (
+return (
     <div className='bg-zinc-800 flex flex-1 items-center justify-end md:justify-between'>
         <nav>
         <ul className='flex items-center gap-6 text-sm'>
             <li>
                 <Link className='font-semibold text-2xl text-white' href="/">Hjem</Link>
             </li>
-            <span className='text-white'> | </span>
+            <span className='text-white text-2xl'> | </span>
             <li>
                 <Link className='font-semibold text-2xl text-white' href="/prosjekter">prosjekter</Link>
             </li>
-            <span className='text-white'> | </span>
+            <span className='text-white text-2xl'> | </span>
             <li>
                 <Link className='font-semibold text-2xl text-white' href="/om_meg">om meg</Link>
             </li>
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Link from 'next/link'  
 import content from '../public/content/index'
-import Image from 'next/image'
-import img from '../public/assets/bg.jpg'
 
 const index = () => (
   <div className='indexdiv min-h-screen bg-zinc-800'>
-  {/* <Image className='profilepic border-slate-800' layout='fill' src={img} alt="bg"/> */}
-    <h1 className='text-center text-white  font-bold text-3xl'>{content.no.text[2]}</h1>
+    <h1 className='align-middle text-center text-white  font-bold text-3xl'>{content.no.text[2]}</h1>
     <h1 className='text-center text-xl font-semibold text-white'>{content.no.text[1]} {content.no.text[0]}</h1>
   </div>
 )
