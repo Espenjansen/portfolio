233f0d26fa9b5ef7bba1dabbd9fd1004e57ebeee
Espenjansen <ejansen1201@gmail.com>
2022-12-15 21:30:56 +0100

    update

--- portfolio/pages/teach_me.tsx
+++ portfolio/pages/teach_me.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import content from '../public/content'
+import imagepage from '../public/assets/img_page.jpg'
+import ui from '../public/assets/ui.jpg'
+import Image from 'next/image'
 
 const teach_me = () => {
 return (
@@ -9,6 +12,12 @@ return (
         <p className='text-white text-center'>{content.no.teach_me[1]}</p>
         <p className='text-white text-center'>{content.no.teach_me[2]}</p>
         <p className='text-white text-center'>{content.no.teach_me[3]}</p>
+        <br />
+        <p className='text-white text-center'>{content.no.teach_me[5]}</p>
+        <center><Image className='border-slate-800 w-2/4' src={imagepage} alt={content.no.teach_me[4]} /></center>
+        <br />
+        <p className='text-white text-center'>{content.no.teach_me[6]}</p>
+        <center><Image className='border-slate-800 w-2/4' src={ui} alt={content.no.teach_me[4]} /></center>
     </div>
 )
 }
--- portfolio/public/content/index.js
+++ portfolio/public/content/index.js
@@ -1,12 +1,26 @@
 export default{
     no:{
         text: ["Espen Storstein Jansen", "Jeg er", "Dette er min portfolio"],
+        
         kontakt_meg: ["mobil nummer: +47 98896115","mail: ejansen1201@gmail.com"],
+        
         om_meg:["Om meg","Jeg heter Espen Storstein Jansen og går på Haugaland vgs på IT  jeg kommer fra Haugesund og er 17 år.", "bilde av meg"],
+        
         prosjekter:["Dette er mine prosjekter","Redwood Js","Teach me","Django prosjekt","Django","Maya prosjekt","Maya", "Python prosjekt", "Python"],
-        teach_me:["Dette er mitt Redwood JS prosjekt","Under dette prosjekter gjorde jeg både backend og litt front.", "I backend så Gjorde jeg at vi kunne legge til bilder til en database.", "Vi lagret bildene i File stack"],
+
+        teach_me:["Dette er mitt Redwood JS prosjekt",
+            "Under dette prosjekter gjorde jeg både backend og litt front.", 
+            "I backend så Gjorde jeg at vi kunne legge til bilder til en database.", 
+            "Vi lagret bildene i File stack", 
+            "Bilde av siden du kan bruke til å laste opp bilder.", 
+            "Under er et bilde av siden du ser da du skal laste opp bilder",
+            "Under ser du et bilde av ui-en i på nettsiden som er lagd av Patrick Jøssang",
+            "Bilde av ui-en"],
+
         django:["Dette er mitt Django prosjekt", "Dette prosjektet mitt er en handleliste/todo-liste.", " Den er lagd i Django og jeg bruker flere tables for å lagre brukere og for å lagre todo listene til en bruker og alle tingene den brukeren har skrevet."],
+
         navbar:["Hjem", "Prosjekter", "Om meg", "Kontakt meg"],
+
         maya:["Dette er mitt Maya prosjekt", "Maya er et 3D program som blir brukt til ting som spill og videoer.",
             "Jeg så en Tutorial og gjorde det etter på han hadde gjort det.",
             "Jeg synes jeg lærte mye av dette på grunn av jeg fikk gjøre det selv etterpå.",
@@ -24,6 +38,7 @@ export default{
             "Til denne brukte jeg CV curve tool til å lage en kan som jeg kopierte opp",
             "dro den ene til siden og lagde sånn at d er noe i mellom kantene på arket.",
             "Bilde av et ark."],
+
         python:["Dette er mitt python prosjekt",
             "Jeg hadde om python gjennom denne dybdelæringen lagde jeg flere små prosjekter som du kan se under.",
             "Blackjack",
