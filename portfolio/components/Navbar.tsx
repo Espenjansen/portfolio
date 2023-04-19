50dcefa9e81d753ed14681facf853a9915bc7ce0
Espenjansen <ejansen1201@gmail.com>
2023-04-19 12:58:40 +0200

    Update Navbar.tsx

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 return (
     <div className='bg-zinc-800 left-1 flex flex-1 items-center justify-end md:justify-between'>
         <nav>
-            <ul className='flex items-center gap-6 text-sm'>
+            <ul className='flex items-center py-2 gap-4 text-sm'>
                 <li>
                     <Link className='font-semibold text-2xl text-white' href="/">{content.no.navbar[0]}</Link>
                 </li>
